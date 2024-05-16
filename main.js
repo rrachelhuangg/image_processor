e6f22c8877b58c67f11512db96ff8e704ef7d4b8
Rachel Huang <rachelhuang505@gmail.com>
2024-05-15 22:35:00 -0400

    added a canvas element for testing purposes

--- main.js
+++ main.js
@@ -15,9 +15,35 @@ const renderFile = () => {
     if(file){
       reader.readAsDataURL(file);
     }
+
 } 
 
 const removeFile = () => {
   const render = document.getElementById('uploadedPhoto')
   render.src = "imgs/upload.png";
 } 
+
+//
+
+function identityKernel(width, height, centerX, centerY){
+  let weightArray = [];
+  for(var h = 0; h < height; h++){
+    weightArray.push([]);
+    for(var w = 0; w < width; w++){
+      weightArray[h].push(0);
+    }
+  }
+}
+
+const test = () => {
+  const imageElement = document.getElementById("uploadedPhoto");
+  var width  = imageElement.naturalWidth;
+  var height = imageElement.naturalHeight;   
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(imageElement, 0, 0);
+  var image = ctx.getImageData(0, 0, width, height);
+  var pix = image.data;
+} 
+
+
