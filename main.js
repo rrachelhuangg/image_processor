5b22e43144ef647126b6f41a604dfd4788964bf8
Rachel Huang <rachelhuang505@gmail.com>
2024-05-17 20:28:40 -0400

    got kernels working!

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // python -m http.server
+//https://blog.j2i.net/2017/11/25/kernel-filters-in-htmljavascript/
+//https://stackoverflow.com/questions/70221084/solved-get-neighboring-pixels-from-linear-array-as-in-context-getimagedata
+//https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+//https://stackoverflow.com/questions/17764012/image-being-clipped-when-copied-to-html-canvas-using-drawimage
+//
 const uploadButton = document.getElementById("uploadButton")
 const removeButton = document.getElementById("removeButton")
 
@@ -38,7 +43,7 @@ function downloadImage(){
 const invert = () => {
   const img = new Image();
   img.crossOrigin = "anonymous";
-  img.src = "imgs/upload.png";
+  img.src = "imgs/city.png";
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext('2d');
   ctx.drawImage(img, 0, 0);
@@ -53,35 +58,68 @@ const invert = () => {
   ctx.putImageData(imageData,0, 0);
 } 
 
-function nbrPixels(pixelIdx, imageData, data){ //gets the neighboring pixels for a pixel
-  nbrs = [] // clear array
-  nbrs[0] = data[pixelIdx - imageData.width * 4 - 4] // Upper left
-  nbrs[1] = data[pixelIdx - imageData.width * 4]     // Upper middle
-  nbrs[2] = data[pixelIdx - imageData.width * 4 + 4] // Upper right
-  nbrs[3] = data[pixelIdx - 4] // left
-  nbrs[4] = data[pixelIdx + 4] // right
-  nbrs[5] = data[pixelIdx + imageData.width * 4 - 4] // Lower left
-  nbrs[6] = data[pixelIdx + imageData.width * 4]     // lower middle
-  nbrs[7] = data[pixelIdx + imageData.width * 4 + 4] // Lower right
-  return nbrs;
-}
-
-function test(){
-  let edKernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
+function grayscale(){
   const img = new Image();
   img.crossOrigin = "anonymous";
-  img.src = "imgs/dog.jpg";
+  img.src = "imgs/city.jpg";
   var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
+function nbrPixels(pixelIdx, imageData, data){ //idx values of neighboring pixels
+  let nbrIdxs = [];
+  nbrIdxs[0] = pixelIdx - imageData.width * 4 - 4; //upper left
+  nbrIdxs[1] = pixelIdx - imageData.width * 4; //upper middle
+  nbrIdxs[2] = pixelIdx - imageData.width * 4 + 4; //upper right
+  nbrIdxs[3] = pixelIdx - 4; //left
+  nbrIdxs[4] = pixelIdx + 4; //right
+  nbrIdxs[5] = pixelIdx + imageData.width * 4 - 4; //lower left
+  nbrIdxs[6] = pixelIdx + imageData.width * 4; //lower middle
+  nbrIdxs[7] = pixelIdx + imageData.width * 4 + 4; //lower right
+  return nbrIdxs;
+}
+
+function applyKernel(kernel){
+  const img = new Image();
+  img.crossOrigin = "anonymous";
+  img.src = "imgs/city.jpg";
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   var data = imageData.data;
   for(let i = 0; i < data.length; i+=4){
-    let nbrs = nbrPixels(i, imageData, data);
-    for(let j = 0; j < nbrs.length; j++){
-      data[i] = data[i] + (nbrs[j]*edKernel[j]);
+    let nbrIdxs = nbrPixels(i, imageData, data);
+    data[i] = data[i] * kernel[8];
+    data[i+1] = data[i+1] * kernel[8];
+    data[i+2] = data[i+2]*kernel[8];
+    for(let n = 0; n < nbrIdxs.length; n++){
+      let nbrIdx = nbrIdxs[n];
+      data[i] = data[i] + (data[nbrIdx]*kernel[n]); //is a temp imageData/data needed?
+      data[i+1] = data[i+1] + (data[nbrIdx+1]*kernel[n]);
+      data[i+2] = data[i+2] + (data[nbrIdx+2] * kernel[n]);
     }
   }
-  ctx.putImageData(imageData,0, 0);
+  ctx.putImageData(imageData, 0, 0);
+}
+
+function edgeDetection(){ 
+  let edgeDetectionKernel = [-1, -1, -1, -1, -1, -1, -1, -1, 8]; //formatted so that the center pixel is at the end
+  applyKernel(edgeDetectionKernel);
+}
+
+function embossing(){
+  let embossKernel = [-2,-1,0,-1,1,0,1,2,1]; 
+  applyKernel(embossKernel);
 }
 
